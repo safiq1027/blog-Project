6e8b6d9109c5790e932a83c96013e6cf68f702d3
Md Safiqul Islam <112417622+safiq1027@users.noreply.github.com>
2023-10-28 15:10:26 +0600

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,9 +1,27 @@
 <script setup></script>
 <template>
+<div class="contact">
     <h1 class="text-2xl mb-3"> This is contact page</h1>
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam doloremque natus eligendi. Dicta ullam obcaecati quidem blanditiis, illum minus possimus earum adipisci sunt accusantium autem. Saepe molestias quaerat enim. Autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil delectus, perspiciatis ex sequi dolorum nesciunt illo quos harum. Odit, id deleniti rerum qui praesentium ipsa? Eius ea accusamus cum repudiandae modi laudantium tenetur debitis iure minus. Architecto placeat quam sapiente nulla sunt magni illum harum ipsum earum voluptatum nobis odit consequatur iste vitae tempore, impedit perferendis aut unde ab iusto mollitia distinctio! Perferendis odit autem similique aliquam nobis quas? Provident consequatur mollitia corporis, cum sequi porro voluptatem nulla quo placeat. Praesentium sit neque dolorem dolores quo. Sapiente saepe doloremque amet ad, vel eum aspernatur sunt molestiae, aut dolorum laboriosam tempore? Maiores eum necessitatibus quod! Velit, ipsam aliquid? Voluptatum, aspernatur! Sint id minima doloribus earum nam cum beatae ea! Aliquam, perferendis id. Vero libero sed dolore! Minus asperiores amet, sint et, laborum saepe aliquam repellendus officia, rem nisi est unde a! Amet sed qui aperiam provident eius ipsam consectetur suscipit error sit libero rem minus, incidunt voluptate voluptatem quod dolore hic iusto porro debitis quasi! Sint dolores tempora, quaerat quisquam facere, laborum molestias perspiciatis dolorem libero quae voluptates aliquam, mollitia autem nostrum obcaecati. Tenetur, eum libero alias illum culpa repellat accusantium id nisi similique hic! Explicabo cupiditate exercitationem minima voluptatum molestias.
     </p>
+</div>
 </template>
 
-<style></style>
+<style scoped>
+
+.contact{
+    background-color: aliceblue;
+    height: 100%;
+    width: 100%;
+
+}
+h1{
+    background-color: darkgray;
+    padding: 1rem;
+    text-align: center;
+}
+p{
+    padding: 1rem;
+}
+</style>
