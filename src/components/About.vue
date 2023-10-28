b079e7f4a8797fab7cbcc7d36e2d2768f8d5c4b7
Md Safiqul Islam <112417622+safiq1027@users.noreply.github.com>
2023-10-28 15:07:17 +0600

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,28 @@
 <!-- //create home component -->
 <script setup></script>
 <template>
+    <div class="about">
     <h1 class="text-2xl mb-3">This is about page</h1>
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam doloremque natus eligendi. Dicta ullam obcaecati quidem blanditiis, illum minus possimus earum adipisci sunt accusantium autem. Saepe molestias quaerat enim. Autem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non atque, maxime et, similique repudiandae eos debitis dolorum quisquam veritatis ipsum exercitationem quia itaque vel dignissimos ullam voluptatibus vitae ipsa dolores nostrum? Ullam, assumenda perspiciatis aspernatur praesentium asperiores iste nulla accusantium iusto voluptatum iure ex facere sed mollitia sint quos obcaecati! Ipsa culpa amet quae sequi, consectetur possimus veniam ipsam? Temporibus, error? Voluptate, fugit! Fugiat quae autem doloremque minus laudantium exercitationem vitae, unde expedita ea numquam libero explicabo necessitatibus temporibus veniam accusamus delectus non quo pariatur sint. Quod quisquam perferendis fugit placeat! Doloribus, tempora nesciunt eligendi nam voluptate facere alias delectus molestiae sit similique asperiores. Velit repellendus quasi ipsam iste dicta fugiat ad architecto quibusdam sapiente natus amet, voluptatum autem ut quisquam repudiandae similique reprehenderit quae dolorum distinctio consequuntur tempora corporis qui expedita pariatur. Consectetur, quae omnis? Et inventore perferendis explicabo beatae necessitatibus saepe error soluta eaque sed deleniti facere maiores facilis repellat obcaecati, veritatis corrupti autem eius temporibus commodi possimus aliquid illum? Officiis eaque cupiditate qui, distinctio voluptas pariatur sed ea excepturi. Amet velit odit facilis itaque, accusamus ad quisquam impedit quia culpa aliquam suscipit, eos omnis explicabo natus possimus aperiam praesentium quidem nostrum architecto odio, asperiores minima nihil?   
     </p>
+</div>
 </template>
 
-<style></style>
+<style scoped>
+
+.about{
+    background-color: aliceblue;
+    height: 100%;
+    width: 100%;
+
+}
+h1{
+    background-color: darkgray;
+    padding: 1rem;
+    text-align: center;
+}
+p{
+    padding: 1rem;
+}
+</style>
